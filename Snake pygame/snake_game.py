014bc91524cda62a1b36157d218c55db946c800e
Bunny <varun.verma2020@vitstudent.ac.in>
2023-10-06 11:13:04 +0530

    added the skeleton agent python file. Will start coding the agent's sub functions now

--- Snake pygame/snake_game.py
+++ Snake pygame/snake_game.py
@@ -94,7 +94,7 @@ class SnakeGameAI:
         game_over = False
 
         # note that we added new condition for the agent. To avoid it doing nothing for a long time, we'll end the game (giving it a negative point for doing nothing (not eating))
-        if self._is_collision() or self.frame_iteration > 100*len(self.snake) :
+        if self.is_collision() or self.frame_iteration > 100*len(self.snake) :
             game_over = True
             reward = -10
             return reward,game_over, self.score
@@ -113,7 +113,7 @@ class SnakeGameAI:
         # 6. return game over and score
         return reward, game_over, self.score
     
-    def _is_collision(self, pt=None):
+    def is_collision(self, pt=None):
         if pt is None : 
             pt = self.head
         # hits boundary
